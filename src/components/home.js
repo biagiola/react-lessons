d373ae812f12b5dcfb0f2d6612e4d3a93892d66f
biagiola <41311088+biagiola@users.noreply.github.com>
2019-09-14 13:18:38 -0400

    Add files via upload

--- src/components/home.js
+++ src/components/home.js
@@ -1,12 +1,32 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
-const home = () => {
-    return (
-        <div>
-            <h1>Home</h1>
-            <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
-        </div>
-    )
+class Home extends Component  {
+    render(){
+        const { posts } = this.props;
+        const postList = posts.length ? ( 
+            posts.map( post => {
+                return (
+                    <div className='container'>
+                        <div>{ post.title }</div>
+                        <div>{ post.body }</div><br/>
+                    </div>
+                )
+            })
+        ) : (
+            <div>No posts was found</div>
+        )
+        return (
+            <div>
+                <h1>Home</h1>
+                { postList }
+            </div>
+        );
+    }
 }
-
-export default home;
+const mapStateToProps = (state) => {
+    return {
+        posts: state.posts
+    }
+}
+export default connect(mapStateToProps)(Home);
