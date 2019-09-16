00397e4208207c0ef433acdadd9887f4590ecdfb
biagiola <41311088+biagiola@users.noreply.github.com>
2019-09-16 12:21:50 -0400

    Delete 2
    In this case we delete a post by 'DELETE_POST' action but we Link to each individual post page and there we deleted the post if we wish.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
-import logo from './logo.svg';
 import './App.css';
+
 import Home from './components/home';
 import About from './components/about';
 import Services from './components/services';
 import Navbar from './components/navbar';
+import Post from './components/Post';
 
 function App() {
   return (
@@ -16,10 +17,10 @@ function App() {
                 <Route exact path='/' component={ Home }/>
                 <Route path='/about' component={ About }/>
                 <Route path='/services' component={ Services }/>
+                <Route path='/:post_id' component={ Post }/>
             </Switch>
         </BrowserRouter>
     </div>
   );
 }
-
 export default App;
